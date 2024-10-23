afcd2b9e80b60b664fc65925460601eead7a585e
Adi <adityabeborta@gmail.com>
2024-10-23 20:41:07 +0530

    Changes related to in-memory authentication

---
+++ src/main/java/com/learn/security/config/SpringSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.learn.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(Customizer -> Customizer.disable());
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return httpSecurity.build();
+    }
+
+    //we are saying not to read user details from application properties but from in memory
+    public UserDetailsService userDetailsService(){
+
+        return new InMemoryUserDetailsManager();
+    }
+}
---
+++ src/main/java/com/learn/security/controller/ApiController.java
@@ -0,0 +1,22 @@
+package com.learn.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    //endpoint one
+    @GetMapping("/route1")
+    public String route1(){
+        return "This is for protected route 1";
+    }
+
+    //endpoint two
+    @GetMapping("/route2")
+    public String route2(){
+        return "This is for protected route 2";
+    }
+}
