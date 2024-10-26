42be79feea74c0365fb27be9031f7f99fa1d5b67
Adi <adityabeborta@gmail.com>
2024-10-26 14:09:36 +0530

    Changes related to Authenticate user from DB with password encoder with login form with removal of configs inside security config

--- src/main/java/com/learn/security/config/SpringSecurityConfig.java
+++ src/main/java/com/learn/security/config/SpringSecurityConfig.java
@@ -3,14 +3,11 @@ package com.learn.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,14 +16,15 @@ public class SpringSecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(Customizer -> Customizer.disable());
-        httpSecurity.formLogin();
-        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return httpSecurity.build();
-    }
+//    @Bean
+//    //overriding the default security filter chain
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.csrf(Customizer -> Customizer.disable());
+//        httpSecurity.httpBasic();
+//        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+////        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        return httpSecurity.build();
+//    }
 
     //we are saying not to read user details from application properties but from in memory
     //with this code we cannot achieve the authentication from DB, so we will need to comment this code
@@ -37,12 +35,16 @@ public class SpringSecurityConfig {
 //        return new InMemoryUserDetailsManager(user1,user2);
 //    }
 
+    //    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+//        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+//        return daoAuthenticationProvider;
+//    }
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        return daoAuthenticationProvider;
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/learn/security/services/impl/UserServiceImplementation.java
+++ src/main/java/com/learn/security/services/impl/UserServiceImplementation.java
@@ -1,21 +1,23 @@
 package com.learn.security.services.impl;
+
 import com.learn.security.entity.Users;
 import com.learn.security.repository.UserRepository;
 import com.learn.security.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImplementation implements UserService {
 
-    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public Users registerUser(Users users) {
-        users.setPassword(this.bCryptPasswordEncoder.encode(users.getPassword()));
+        users.setPassword(this.passwordEncoder.encode(users.getPassword()));
         return this.userRepository.save(users);
     }
 }
