a4a87284abde2ed1e739c863b1b44a1cc0cb8802
Adi <adityabeborta@gmail.com>
2024-10-26 11:51:01 +0530

    Changes related to Authenticate user from DB with password encoder with login form

--- src/main/java/com/learn/security/config/SpringSecurityConfig.java
+++ src/main/java/com/learn/security/config/SpringSecurityConfig.java
@@ -9,18 +9,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SpringSecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
@@ -28,9 +22,9 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(Customizer -> Customizer.disable());
+        httpSecurity.formLogin();
         httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        httpSecurity.httpBasic(Customizer.withDefaults());
-        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
     }
 
--- src/main/java/com/learn/security/controller/ApiController.java
+++ src/main/java/com/learn/security/controller/ApiController.java
@@ -4,19 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
     //endpoint one
     @GetMapping("/route1")
-    public String route1(){
-        return "This is for protected route 1";
+    public String route1(Principal principal) {
+        return "This is for protected route 1 " + principal.getName();
     }
 
     //endpoint two
     @GetMapping("/route2")
-    public String route2(){
+    public String route2() {
         return "This is for protected route 2";
     }
 }
